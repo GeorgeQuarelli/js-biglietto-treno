87a66f92cd8d426b0dd472a2122dbaa40bc1ba88
George <quarelli.george@gmail.com>
2020-04-20 19:45:22 +0200

    fix

--- main.js
+++ main.js
@@ -1,24 +1,24 @@
 // chiedere quanti chilometri vuole fare
 var chilometri = prompt('quanti chilometri vuoi fare?')
-console.log(chilometri + 'chilometri');
+console.log(chilometri + ' chilometri');
 // chiedere quanti anni ha l'utente
 var anni = prompt('quanti anni hai?')
-console.log(anni + 'anni');
+console.log(anni + ' anni');
 
 //calcolo prezzo chilometrio
 var costo_chilometri = (chilometri * 0.21);
-console.log('costo dei chilometri' + costo_chilometri + '€');
+console.log('costo dei chilometri ' + costo_chilometri + '€');
 // calcolo sconto
 if(anni < 18) {
     var sconto_minorenni = (costo_chilometri * 20 / 100)
-    console.log(sconto_minorenni + '€ di sconto (20%)');
+    console.log(sconto_minorenni + ' € di sconto (20%)');
     var costo_totale1 = (costo_chilometri - sconto_minorenni)
     console.log('costo totale :' + costo_totale1 + '€');
 } else if(anni >= 65) {
     var sconto_over = (costo_chilometri * 40 / 100)
     console.log(sconto_over + '€ di sconto (40%)');
     var costo_totale2 = (costo_chilometri - sconto_over)
-    console.log('costo totale :' + costo_totale2 + '€');
+    console.log('costo totale: ' + costo_totale2 + '€');
 } else {
     console.log('non hai nessuno sconto');
 }
